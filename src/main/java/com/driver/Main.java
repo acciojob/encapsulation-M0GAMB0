023b29418d351a77e8c0b5306efc4637bbd8d545
Saikumar Hangal⚡ <saikumarhangal@gmail.com>
2023-03-17 13:09:52 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+   public static void main(String args[]) {
+        RWOnly obj=new RWOnly();
+        //obj.name="Saikumar";
+        //Main.java:4: error: name has private access in RWOnly
+        obj.setter("Saikumar");
+        System.out.println(obj.getter());
+    }
 }
